98215fb0945257ee2df99b72e9be378db6d88383
unknown <Amiram.ab@gmail.com>
2022-09-28 09:48:26 +0300

    9.1 Done

---
+++ JS/FullStack-Bootcamp-Week4/9.1-loop_in_a_loop/app.js
@@ -0,0 +1,9 @@
+const listOfNeighbours = [
+    ["Canada", "Mexico"],
+    ["Spain"],
+    ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    const element = listOfNeighbours[i];
+}
