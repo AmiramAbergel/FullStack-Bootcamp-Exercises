7af4787116ba7efaa266256a21d3a5eadabeacec
unknown <Amiram.ab@gmail.com>
2022-09-29 11:50:51 +0300

    11.1.1 done

---
+++ JS/FullStack-Bootcamp-Week4/11.1-map and forEach/app.js
@@ -0,0 +1,44 @@
+const newReleases = [
+    {
+        id: 70111470,
+        title: "Die Hard",
+        boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [4.0],
+        bookmark: [],
+    },
+    {
+        id: 654356453,
+        title: "Bad Boys",
+        boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [5.0],
+        bookmark: [{ id: 432534, time: 65876586 }],
+    },
+    {
+        id: 65432445,
+        title: "The Chamber",
+        boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [4.0],
+        bookmark: [],
+    },
+    {
+        id: 675465,
+        title: "Fracture",
+        boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [5.0],
+        bookmark: [{ id: 432534, time: 65876586 }],
+    },
+];
+
+const arrayLoop = (arr) => {
+    const res = [];
+    arr.forEach((element) => {
+        res.push({ id: element.id, name: element.title });
+    });
+    console.log(res);
+};
+
+arrayLoop(newReleases);
