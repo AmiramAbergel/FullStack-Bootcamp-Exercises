593f4beb6e78ada30442a55e3f7cb9083b15877b
unknown <Amiram.ab@gmail.com>
2022-09-29 14:08:08 +0300

    11.2 Done

--- JS/FullStack-Bootcamp-Week4/11.2-forEach&Mapping/app.js
+++ JS/FullStack-Bootcamp-Week4/11.2-forEach&Mapping/app.js
@@ -1,7 +1,7 @@
 //1
 
 const numbers = [23, 12, 423, 6, 3, 76, 3];
-const string = "sasd SADAS aszxca AS#s212 sxcv 5ggsPING";
+const string = "sqsd SADAS aszxca AS#s212 sxcv 5ggsPING";
 
 const doubleValues = (arr) => {
     const doubles = arr.map((num) => (num = num * 2));
@@ -74,19 +74,45 @@ const vowelCount = (str) => {
 };
 
 //Test4
-vowelCount(string);
+//vowelCount(string);
 
 //5
-const capitalize = (str) => {};
+const capitalize = (str) => {
+    let arr = str.split("");
+    arr.forEach((element, index) => {
+        arr[index] = element.toUpperCase();
+    });
+    return arr.join(" ");
+};
 
 //Test5
+//capitalize(string);
 
 //6
-const shiftLetters = (str) => {};
+const shiftLetters = (str) => {
+    let res = str.split("");
+    res.forEach((element, index) => {
+        charcode = element.charCodeAt() - 1;
+        res[index] = String.fromCharCode(charcode);
+    });
+    return res.join(" ");
+};
 
 //Test6
+//shiftLetters(string);
 
 //7
-const swapCase = (str) => {};
+const swapCase = (str) => {
+    let arr = str.split("");
+    arr.forEach((element, index) => {
+        if (index % 2 !== 0) {
+            arr[index] = element.toUpperCase();
+        } else {
+            arr[index] = element.toLowerCase();
+        }
+    });
+    return arr.join(" ");
+};
 
 //Test 7
+swapCase(string);
