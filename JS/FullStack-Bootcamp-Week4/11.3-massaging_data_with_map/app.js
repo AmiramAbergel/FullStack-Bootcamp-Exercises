6126f303fadc5f32ead75a4f227a8e7cb007f1cf
unknown <Amiram.ab@gmail.com>
2022-09-29 14:40:20 +0300

    11.3.2 Done

---
+++ JS/FullStack-Bootcamp-Week4/11.3-massaging_data_with_map/app.js
@@ -0,0 +1,68 @@
+const data = [
+    {
+        name: "John",
+        birthday: "1-1-1995",
+        favoriteFoods: {
+            meats: ["hamburgers", "sausages"],
+            fish: ["salmon", "pike"],
+        },
+    },
+    {
+        name: "Mark",
+        birthday: "10-5-1980",
+        favoriteFoods: {
+            meats: ["hamburgers", "steak", "lamb"],
+            fish: ["tuna", "salmon", "barracuda"],
+        },
+    },
+    {
+        name: "Mary",
+        birthday: "1-10-1977",
+        favoriteFoods: {
+            meats: ["ham", "chicken"],
+            fish: ["pike"],
+        },
+    },
+    {
+        name: "Thomas",
+        birthday: "1-10-1990",
+        favoriteFoods: {
+            meats: ["bird", "rooster"],
+            fish: ["salmon"],
+        },
+    },
+    {
+        name: "Mary",
+        birthday: "1-10-1977",
+        favoriteFoods: {
+            meats: ["hamburgers", "lamb"],
+            fish: ["anchovies", "tuna"],
+        },
+    },
+];
+
+//1
+const namesFromArr = (arr) => {
+    const namesArr = [];
+    arr.forEach((element) => {
+        namesArr.push(element.name);
+    });
+    return namesArr;
+};
+
+//Test1
+//namesFromArr(data);
+
+//2
+const objBorn90sFromArr = (arr) => {
+    const obj90sArr = [];
+    arr.forEach((element, index) => {
+        if (parseInt(element.birthday.split("-")[2]) < 1990) {
+            obj90sArr.push(element);
+        }
+    });
+    return obj90sArr;
+};
+
+//Test2
+objBorn90sFromArr(data);
