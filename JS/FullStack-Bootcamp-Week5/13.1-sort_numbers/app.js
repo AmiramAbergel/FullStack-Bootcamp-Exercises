9e6a4c3ab7b167efa0ab59fb041c5d44d4c86459
unknown <Amiram.ab@gmail.com>
2022-10-02 12:06:50 +0300

    13.1 Done

--- JS/FullStack-Bootcamp-Week5/13.1-sort_numbers/app.js
+++ JS/FullStack-Bootcamp-Week5/13.1-sort_numbers/app.js
@@ -1,3 +1,9 @@
 const numbers = [1, -5, 666, 2, 400, 11];
 
-//1
+//1 - Descending to ascending order.
+const sort1 = numbers.sort((a, b) => a - b);
+console.log(sort1); // Test
+
+//2 - Descending to descending order.
+const sort2 = numbers.sort((a, b) => b - a);
+console.log(sort2); // Test
