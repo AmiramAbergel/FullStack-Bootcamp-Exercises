fc18e52291ebed38a0f38d728bd0d1de563d49c5
unknown <Amiram.ab@gmail.com>
2022-10-11 18:38:32 +0300

    19.1 Done

--- JS/FullStack-Bootcamp-Week6/19.1-dom_walk/script.js
+++ JS/FullStack-Bootcamp-Week6/19.1-dom_walk/script.js
@@ -1,2 +1,16 @@
 const mainTitle = document.querySelector(".start-here");
 mainTitle.textContent = "main title";
+//
+const locate = mainTitle.nextElementSibling.firstElementChild;
+const newSub = document.createElement("li");
+newSub.innerText = "subtitle 4";
+locate.appendChild(newSub);
+console.dir(locate);
+//
+const lastLi = mainTitle.parentElement.lastElementChild.remove();
+//
+const title = document.querySelector("title");
+title.innerText = "Master Of The Dom";
+//
+const p = document.querySelector("div p");
+p.innerText = "lorem";
