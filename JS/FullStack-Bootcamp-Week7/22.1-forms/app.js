220c14184ddba9b0f798fef7dce41f179b23ffdd
unknown <Amiram.ab@gmail.com>
2022-10-18 12:43:09 +0300

    22.1 Done

--- JS/FullStack-Bootcamp-Week7/22.1-forms/app.js
+++ JS/FullStack-Bootcamp-Week7/22.1-forms/app.js
@@ -3,19 +3,41 @@ const form = document.querySelector("#user-form");
 const userName = document.querySelector("#name");
 const userAge = document.querySelector("#age");
 const userMail = document.querySelector("#email");
-
 const formData = {};
 
+const displayFormData = () => {
+    alert(
+        "Your Info:\n" +
+            formData.userName +
+            "\n" +
+            formData.userAge +
+            "\n" +
+            formData.userMail +
+            "\n"
+    );
+};
+
+for (let input of [userName, userAge, userMail]) {
+    input.addEventListener("input", (e) => {
+        formData[e.target.name] = e.target.value;
+    });
+}
+
 form.addEventListener("submit", function (e) {
-    e.preventDefault();
+    displayFormData(formData);
+    if (window.confirm(`Is the information you provided accurate?`)) {
+        alert("Congratulations you successfully sent this form");
+    } else {
+        e.preventDefault();
+    }
 });
 
-userName.addEventListener("input", (e) => {
-    formData["userName"] = e.target.value;
-});
-userAge.addEventListener("input", (e) => {
-    formData["userAge"] = e.target.value;
-});
-userMail.addEventListener("input", (e) => {
-    formData["userMail"] = e.target.value;
-});
+// userName.addEventListener("input", (e) => {
+//     formData["userName"] = e.target.value;
+// });
+// userAge.addEventListener("input", (e) => {
+//     formData["userAge"] = e.target.value;
+// });
+// userMail.addEventListener("input", (e) => {
+//     formData["userMail"] = e.target.value;
+// });
