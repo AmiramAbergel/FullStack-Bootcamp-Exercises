2856b67eebbd2370bc76f461fda63e13d3aa4cef
unknown <Amiram.ab@gmail.com>
2022-10-23 17:46:35 +0300

    23.1 Done

--- JS/FullStack-Bootcamp-Week8/23.1-fundamentals_of_this/app.js
+++ JS/FullStack-Bootcamp-Week8/23.1-fundamentals_of_this/app.js
@@ -4,6 +4,12 @@
  * (Note this is the global scope)
  */
 console.log(this);
+/*
+* Ans: 
+this - will point to window. because window is the current execution and this inside global scope is a reference to an object- in our case: window in the browser.
+this point to the owner to the of the function.
+*/
+
 //---------------------------------------------------//
 /*
  * Question 2:
@@ -17,6 +23,14 @@ const myObj = {
     },
 };
 myObj.greet();
+
+/*
+* Ans:
+this will point to window again because of the arrow function.
+Arrow function does not have its own this.
+* Fix:
+We will change the arrow function to function declaration.
+*/
 //---------------------------------------------------//
 
 /*
@@ -26,6 +40,12 @@ myObj.greet();
 const myFuncDec = function () {
     console.log(this);
 };
+/*
+* Ans:
+this will point to window again because myFuncDec is in the global scope,
+in our case window browser is the global scope .
+
+*/
 //---------------------------------------------------//
 /*
  * Question 4:
@@ -35,6 +55,12 @@ const myFuncArrow = () => {
     console.log(this);
 };
 myFuncArrow();
+/*
+* Ans: 
+Arrow function does not have its own this.
+In our case this point to arrow function.
+For that case this - point to window.
+*/
 //---------------------------------------------------//
 /*
  * Question 5:
@@ -45,3 +71,10 @@ document.querySelector(".element").addEventListener(() => {
     console.log(this);
 });
 //---------------------------------------------------//
+/*
+* Ans: 
+this will point to window, because there is arrow function that called by "addEventListener".
+* Fix:
+First we should add event like 'click', 2nd we need to store document with
+let/const than "this" (that acts like call "event") will apply on the our new stored variable.
+*/
