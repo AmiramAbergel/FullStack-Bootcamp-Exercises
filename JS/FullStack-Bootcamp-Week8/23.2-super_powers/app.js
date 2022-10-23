cf2cd9535e861c41575537d95554f7d127b80e8f
unknown <Amiram.ab@gmail.com>
2022-10-23 18:03:29 +0300

    23.2 done

--- JS/FullStack-Bootcamp-Week8/23.2-super_powers/app.js
+++ JS/FullStack-Bootcamp-Week8/23.2-super_powers/app.js
@@ -1,6 +1,10 @@
 const storm = {
     // add code here
+    superPower: "flying",
+    printFunc: printSuperPower,
 };
 function printSuperPower() {
     console.log("my superpower is " + this.superPower);
 }
+
+storm.printFunc();
