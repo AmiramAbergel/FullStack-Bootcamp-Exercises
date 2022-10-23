c30825e92d495ec35fc1378c47a7886ed336a37c
unknown <Amiram.ab@gmail.com>
2022-10-23 21:31:33 +0300

    24.3 done (should back to reduce)

--- JS/FullStack-Bootcamp-Week8/24.3-own_methods/app.js
+++ JS/FullStack-Bootcamp-Week8/24.3-own_methods/app.js
@@ -2,12 +2,53 @@
 /*
  * 1. Build your own filter method with the help of prototypes
  */
+Array.prototype.myFilter = function (callback) {
+    const resArr = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            resArr.push(this[i]);
+        }
+    }
+    return resArr;
+};
 
 /*
  * 2. Build your own find method with the help of prototypes
  */
 
+Array.prototype.myFind = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            return this[i];
+        }
+    }
+};
+
 /*
  * Extra:
  * 3. Build your own reduce method with the help of prototypes
  */
+
+Array.prototype.myReduce = function (callback, initialValue = this[0]) {
+    let res = initialValue;
+    for (let i = 1; i < this.length; i++) {
+        if (initialValue === this[0]) {
+            res = callback(this[i - 1], this[i], i, this);
+        } else {
+            res = callback(res, this[i], i, this);
+        }
+    }
+    return res;
+};
+const myArr3 = [1, 2, 3, 4];
+const initialValue = 0;
+
+const sum = myArr3.myReduce((prevVal, currVal) => {
+    return prevVal + currVal;
+}, initialValue);
+
+console.log(sum);
+
+console.log([1, 100].myReduce((a, b) => Math.max(a, b), 50));
+console.log([1, 100].myReduce((a, b) => Math.max(a, b), 101));
+console.log([50].myReduce((a, b) => Math.max(a, b), 10));
