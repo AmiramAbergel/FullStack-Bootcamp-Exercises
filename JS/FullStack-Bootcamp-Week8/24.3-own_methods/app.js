15dd3404ac8ebd2a499c527da4cff37e796067b7
unknown <Amiram.ab@gmail.com>
2022-10-23 13:38:51 +0300

    Exs styled correctly

---
+++ JS/FullStack-Bootcamp-Week8/24.3-own_methods/app.js
@@ -0,0 +1,13 @@
+//Instructions
+/*
+ * 1. Build your own filter method with the help of prototypes
+ */
+
+/*
+ * 2. Build your own find method with the help of prototypes
+ */
+
+/*
+ * Extra:
+ * 3. Build your own reduce method with the help of prototypes
+ */
