6683a194efbba32271e856ccfd6298bd60973ee8
unknown <Amiram.ab@gmail.com>
2022-10-26 10:35:21 +0300

    New daily exs added

--- JS/FullStack-Bootcamp-Week8/29.1-aync_await/app.js
+++ JS/FullStack-Bootcamp-Week8/29.1-aync_await/app.js
@@ -1,37 +1,3 @@
-// const getIDs = () =>
-//     new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             resolve([532, 543, 753, 1, 5]);
-//         }, 1500);
-//     });
-
-// const getRecipe = (recipeID) => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(
-//             (ID) => {
-//                 const recipe = {
-//                     title: "Fresh tomato pasta",
-//                     publisher: "Pinchas Hodadad",
-//                 };
-//                 resolve(`${ID}: ${recipe.title}`);
-//             },
-//             1500,
-//             recipeID
-//         );
-//     });
-// };
-
-// getIDs()
-//     .then((IDs) => {
-//         console.log(IDs);
-//         return getRecipe(IDs[2]);
-//     })
-//     .then((recipe) => {
-//         console.log(recipe);
-//     })
-//     .catch((error) => {
-//         console.log("It is an error!");
-//     });
 const getIDs = () =>
     new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -39,19 +5,21 @@ const getIDs = () =>
         }, 1500);
     });
 
-async function getRecipe(recipeID) {
-    await setTimeout(
-        (ID) => {
-            const recipe = {
-                title: "Fresh tomato pasta",
-                publisher: "Pinchas Hodadad",
-            };
-            return `${ID}: ${recipe.title}`;
-        },
-        1500,
-        recipeID
-    );
-}
+const getRecipe = (recipeID) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(
+            (ID) => {
+                const recipe = {
+                    title: "Fresh tomato pasta",
+                    publisher: "Pinchas Hodadad",
+                };
+                resolve(`${ID}: ${recipe.title}`);
+            },
+            1500,
+            recipeID
+        );
+    });
+};
 
 getIDs()
     .then((IDs) => {
