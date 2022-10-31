75c5a55101e6d2917fe5bcafab84fc58bf8a5bc4
unknown <Amiram.ab@gmail.com>
2022-10-31 11:42:41 +0200

    then,catch done

--- JS/FullStack-Bootcamp-Week9/JS-Recap-day-2/app.js
+++ JS/FullStack-Bootcamp-Week9/JS-Recap-day-2/app.js
@@ -1,6 +1,6 @@
 /** 1)
   Create an input field and a button when the button is clicked 
-  call  function that receives 'number' as a parameter (from input field) 
+  call function that receives 'number' as a parameter (from input field) 
   and returns a new promise after 2 sec
   If the number is above 17 display on the screen 'You can drive' 
   and if it's smaller throw error and display on the screen 'You're too young to drive'
@@ -8,3 +8,38 @@
 //! Check yourself once with .then .catch and once with async await
 //! DRY
 //! Small functions
+const formBtn = document.querySelector(".getNum");
+const formInput = formBtn.querySelector(".input");
+
+window.onload = () => {
+    formInput.focus();
+};
+
+const getNum = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const userInput = form[0].value;
+    if (userInput !== "") {
+        wait(userInput, 2000)
+            .then(() => {
+                console.log("You can drive");
+            })
+            .catch(() => {
+                console.log("You're too young to drive!");
+            });
+    }
+};
+
+function wait(input, milliseconds) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (input > 17) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, milliseconds);
+    });
+}
+
+formBtn.addEventListener("submit", getNum);
