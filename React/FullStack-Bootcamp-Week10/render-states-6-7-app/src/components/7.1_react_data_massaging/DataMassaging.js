0b7c8609aab88b4264ff317917471094fde8142c
unknown <Amiram.ab@gmail.com>
2022-11-07 20:52:25 +0200

    7.1.2 Done

--- React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/components/7.1_react_data_massaging/DataMassaging.js
+++ React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/components/7.1_react_data_massaging/DataMassaging.js
@@ -5,10 +5,19 @@ const DataMassaging = (props) => {
     const db = props.items;
     const arrToJsx = db.map((messege) => {
         // return <li key={messege["favoriteFoods"].fish}> {messege.name}</li>;
-
         return messege.name;
     });
-    console.log(arrToJsx);
+    const traverseObj = (data) => {
+        const obj90sArr = [];
+        const conditionYear = 1990;
+        db.forEach((element) => {
+            if (parseInt(element.birthday.split("-")[2]) < conditionYear) {
+                obj90sArr.push(element);
+            }
+        });
+        return obj90sArr;
+    };
+    console.log(traverseObj(db));
     return arrToJsx;
 };
 
