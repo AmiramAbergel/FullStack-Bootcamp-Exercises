ab05cd080632a389c1fc4b971ba53357276e6eb8
unknown <Amiram.ab@gmail.com>
2022-11-07 16:44:23 +0200

    6.2 Done

--- React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/App.js
+++ React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Counter from "./components/Counter";
+import Counter from "./components/6.1_Increment/Counter";
+import HideSeek from "./components/6.2_hide_and_seek/HideSeek";
 
 function App() {
     return (
         <div className="App">
             <Counter counter={0} />
+            <HideSeek title={"show/hide"} con={true}></HideSeek>
         </div>
     );
 }
---
+++ React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/components/6.2_hide_and_seek/HideSeek.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./HideSeek.css";
+const HideSeek = (props) => {
+    const title = props.title;
+
+    let [toggle, setToggle] = useState(false);
+    const clickHandler = () => {
+        setToggle(!toggle);
+    };
+    return (
+        <div className="container">
+            <button onClick={clickHandler}>{title}</button>
+            <div
+                className="colorContainer"
+                style={{ visibility: toggle ? "visible" : "hidden" }}
+            ></div>
+        </div>
+    );
+};
+
+export default HideSeek;
