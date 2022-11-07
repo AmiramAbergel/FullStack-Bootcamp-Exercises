e514baba2827e830808c265ae372113a3ba1caaf
unknown <Amiram.ab@gmail.com>
2022-11-07 14:45:20 +0200

    6.1 done

--- React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/App.js
+++ React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Counter counter={0} />
+        </div>
+    );
 }
 
 export default App;
--- React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/components/Counter.js
+++ React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/components/Counter.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = (props) => {
+    const [count, setCount] = useState(props.counter);
+
+    const clickHandler = () => {
+        return setCount(count + 1);
+    };
     return (
         <div>
-            <button></button>
+            <button onClick={clickHandler}>increment</button>
+            {count}
         </div>
     );
 };
