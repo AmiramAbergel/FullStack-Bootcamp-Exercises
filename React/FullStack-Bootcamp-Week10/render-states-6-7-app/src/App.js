63ea2ba6d0c4ec6074ccb4dd6b2ece7618f3ff34
unknown <Amiram.ab@gmail.com>
2022-11-07 19:24:50 +0200

    6.3 Done

--- React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/App.js
+++ React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Counter from "./components/6.1_Increment/Counter";
 import HideSeek from "./components/6.2_hide_and_seek/HideSeek";
+import IncrementDecrement from "./components/6.3_increment_and_decrement/IncrementDecrement";
 
 function App() {
     return (
         <div className="App">
             <Counter counter={0} />
             <HideSeek title={"show/hide"} con={true}></HideSeek>
+            <IncrementDecrement counter={0} />
         </div>
     );
 }
--- React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/components/6.3_increment_and_decrement/IncrementDecrement.js
+++ React/FullStack-Bootcamp-Week10/render-states-6-7-app/src/components/6.3_increment_and_decrement/IncrementDecrement.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import "./IncrementDecrement.css";
 
 const IncrementDecrement = (props) => {
+    const [count, setCount] = useState(props.counter);
+    const [color, setColor] = useState("white");
+
+    const incrementHandler = () => {
+        setCount((cn) => {
+            if (cn < 10) {
+                if (props.counter) {
+                }
+                return cn + 1;
+            } else {
+                return cn;
+            }
+        });
+        if (count >= 0) {
+            setColor("posBn");
+        } else if (count === -1) {
+            setColor("zeroBn");
+        } else {
+            setColor("negBn");
+        }
+    };
+    const decrementHandler = () => {
+        setCount((cn) => {
+            if (cn > -10) {
+                return cn - 1;
+            } else {
+                return cn;
+            }
+        });
+        if (count > 1) {
+            setColor("posBn");
+        } else if (count === 1) {
+            setColor("zeroBn");
+        } else {
+            setColor("negBn");
+        }
+    };
+
     return (
         <div>
-            <button></button>
+            <button onClick={incrementHandler}>increment</button>
+            <h2 className={`${color}`}>{count}</h2>
+            <button onClick={decrementHandler}>decrement</button>
         </div>
     );
 };
