f26858873ff928e930df038909bfc19698a0d5da
unknown <Amiram.ab@gmail.com>
2022-11-09 15:31:41 +0200

    10.1 (playPause) - Done

--- React/FullStack-Bootcamp-Week10/use-ref-app/src/components/VideoPlayer/VideoPlayerManager.js
+++ React/FullStack-Bootcamp-Week10/use-ref-app/src/components/VideoPlayer/VideoPlayerManager.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const src = 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4';
 
 const VideoPlayerManager = () => {
+    const videoRef = useRef();
+
+    const videoHandler = async (event) => {
+        const input = event.target;
+        console.dir(input);
+        await videoRef;
+        const videoControl = videoRef.current;
+        console.dir(videoControl);
+        if (input.id === 'pause') {
+            videoControl.pause();
+        } else {
+            videoControl.play();
+        }
+    };
     return (
         <div>
-            <video width='50%'>
+            <video ref={videoRef} width='50%'>
                 <source src={src} type='video/mp4' />
                 Sorry, your browser doesn't support videos.
             </video>
             <p>
-                <button>Play</button>
-                <button>Pause</button>
+                <button onClick={videoHandler} id='play'>
+                    Play
+                </button>
+                <button onClick={videoHandler} id='pause'>
+                    Pause
+                </button>
             </p>
         </div>
     );
