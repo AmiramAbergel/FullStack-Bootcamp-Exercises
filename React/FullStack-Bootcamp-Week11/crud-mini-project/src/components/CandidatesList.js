ec941a42616dfec746e2792650796fd149bb18e6
unknown <Amiram.ab@gmail.com>
2022-11-16 14:59:18 +0200

    Read Done

--- React/FullStack-Bootcamp-Week11/crud-mini-project/src/components/CandidatesList.js
+++ React/FullStack-Bootcamp-Week11/crud-mini-project/src/components/CandidatesList.js
@@ -4,19 +4,25 @@ import Fetch from '../api/Fetch';
 const CandidatesList = (props) => {
     const [candidates, error, isLoading] = Fetch();
 
+    const onRead = () => {
+        const res = candidates.map((candidate) => {
+            return (
+                <div key={candidate.id}>
+                    <h3>
+                        {candidate.first} {candidate.last}
+                    </h3>
+                </div>
+            );
+        });
+        return res;
+    };
     return (
-        <ul>
-            <section>
-                {isLoading && <p>Loading...</p>}
-                {!isLoading && error && <p>{error}</p>}
-                {!isLoading && !error && candidates && (
-                    <p>{candidates[0].first}</p>
-                )}
-                {!isLoading && !error && !candidates && (
-                    <p>Found no candidates</p>
-                )}
-            </section>
-        </ul>
+        <section>
+            {isLoading && <p>Loading...</p>}
+            {!isLoading && error && <p>{error}</p>}
+            {!isLoading && !error && candidates && <>{onRead()}</>}
+            {!isLoading && !error && !candidates && <p>Found no candidates</p>}
+        </section>
     );
 };
 
