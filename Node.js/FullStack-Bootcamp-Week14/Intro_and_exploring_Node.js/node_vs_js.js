0ba980f50814f5d4121f59d380cc0b760c3a4d12
unknown <Amiram.ab@gmail.com>
2022-12-04 15:45:06 +0200

    node vs js Done

---
+++ Node.js/FullStack-Bootcamp-Week14/Intro_and_exploring_Node.js/node_vs_js.js
@@ -0,0 +1,20 @@
+/*
+ * What are 3 things node.js has that the client side javascript doesn’t have. And the reasons why.
+ */
+/*
+ * Ans:
+ * 1. node.js has FS - file sys access,
+ * 2. Global - Node.js global objects are global in nature and they are available in all modules.
+ * 3. Buffer - Buffer class is a global class that can be accessed in an application without importing the buffer module.
+ */
+
+/*
+ *What are 3 things the client side javascript has that node.js doesn’t have. And the reasons why.
+ */
+/*
+ * Ans:
+ * 1. window command-  Node.js doesn't work on the browser & with the DOM... JS is.
+ * 2. local storage
+ * 3. Document.querySelector
+ * 4.  Single-Page Application
+ */
