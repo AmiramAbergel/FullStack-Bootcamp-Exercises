b1b423ae52877c753ccc7d97e42f1eb34cd6780f
unknown <Amiram.ab@gmail.com>
2022-12-07 16:22:16 +0200

    new task added

--- test.js
+++ test.js
@@ -1,11 +1,27 @@
-var isSubsequence = function (s, t) {
-    let cnt = 0;
-    for (let char of t) if (char === s[cnt]) cnt++;
-    return cnt === s.length ? true : false;
+const checkOperation = (str) => {
+    let count = 0;
+    let seen1 = false;
+    // 0001110
+    //00000
+    for (let i = 0; i < str.length; i++) {
+        const element = str[i];
+        if (element === '0' && seen1) {
+            count += 1;
+        } else if (element === '1') {
+            seen1 = true;
+            count += 2;
+        }
+    }
+    return count > 0 ? count - 1 : 0; // operation num
 };
 
+'1'.repeat(400000); // 799999
+('011100'); // 7
+('1011'); // 6
+('1111010101111'); // 22
 //test
 
-isSubsequence('aec', 'abcde');
-isSubsequence('abc', 'abcde');
-isSubsequence('ace', 'abcde');
+console.log(checkOperation('1'.repeat(400000)));
+console.log(checkOperation('011100'));
+console.log(checkOperation('1011'));
+console.log(checkOperation('1111010101111'));
