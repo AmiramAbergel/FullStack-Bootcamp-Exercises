dba2d178b7d09eab85a2d1661799c6670cb564b9
unknown <Amiram.ab@gmail.com>
2022-12-12 09:54:19 +0200

    new daily tasks added

--- Node.js/FullStack-Bootcamp-Week15_Start_MongoDB/15.1_restaurant_collection/app.js
+++ Node.js/FullStack-Bootcamp-Week15_Start_MongoDB/15.1_restaurant_collection/app.js
@@ -11,31 +11,134 @@ client.connect((error, client) => {
         return console.log(`cant connect!`);
     }
     const db = client.db(databaseName);
-    db.collection('restaurants').insertOne(
-        {
-            name: 'Nishi',
-            address: {
-                city: 'Netanya',
-                street: 'Tzafir 15',
-                coordinates: [-72.46574, 42.6774],
+    db.collection('restaurants').insertMany(
+        [
+            {
+                name: 'Nishi',
+                address: {
+                    city: 'Netanya',
+                    street: 'Tzafir 15',
+                    coordinates: [-72.46574, 42.6774],
+                },
+                cuisine: 'Japan',
+                kosher: false,
+                reviews: [
+                    {
+                        date: new Date('2016-01-01'),
+                        score: 7,
+                    },
+                    {
+                        date: new Date('2019-01-01'),
+                        score: 3,
+                    },
+                    {
+                        date: new Date('2018-01-01'),
+                        score: 8,
+                    },
+                ],
             },
-            cuisine: 'Japan',
-            kosher: false,
-            reviews: [
-                {
-                    date: new Date('2016-01-01'),
-                    score: 7,
+            {
+                name: 'Cafe-Cafe',
+                address: {
+                    city: 'Netanya',
+                    street: 'Tzafir 5',
+                    coordinates: [-72.46574, 42.6774],
                 },
-                {
-                    date: new Date('2019-01-01'),
-                    score: 3,
+                cuisine: 'Israel',
+                kosher: true,
+                reviews: [
+                    {
+                        date: new Date('2016-01-01'),
+                        score: 1,
+                    },
+                    {
+                        date: new Date('2014-01-01'),
+                        score: 2,
+                    },
+                    {
+                        date: new Date('2012-01-01'),
+                        score: 3,
+                    },
+                ],
+            },
+            {
+                name: 'Caramel',
+                address: {
+                    city: 'Netanya',
+                    street: 'Hadror 15',
+                    coordinates: [-72.46574, 42.6774],
                 },
-                {
-                    date: new Date('2018-01-01'),
-                    score: 8,
+                cuisine: 'Italy',
+                kosher: false,
+                reviews: [
+                    {
+                        date: new Date('2011-01-01'),
+                        score: 7,
+                    },
+                    {
+                        date: new Date('2012-01-01'),
+                        score: 3,
+                    },
+                    {
+                        date: new Date('2015-01-01'),
+                        score: 8,
+                    },
+                ],
+            },
+            {
+                name: 'Japanika',
+                address: {
+                    city: 'Netanya',
+                    street: 'Tzafir 15',
+                    coordinates: [-72.46574, 42.6774],
                 },
-            ],
-        },
-        () => {}
+                cuisine: 'Japan',
+                kosher: false,
+                reviews: [
+                    {
+                        date: new Date('2015-01-01'),
+                        score: 4,
+                    },
+                    {
+                        date: new Date('2016-01-01'),
+                        score: 3,
+                    },
+                    {
+                        date: new Date('2017-01-01'),
+                        score: 1,
+                    },
+                ],
+            },
+            {
+                name: 'Kyoto',
+                address: {
+                    city: 'Herzlya',
+                    street: 'Tzafir 15',
+                    coordinates: [-72.46574, 42.6774],
+                },
+                cuisine: 'chinese',
+                kosher: false,
+                reviews: [
+                    {
+                        date: new Date('2015-01-01'),
+                        score: 7,
+                    },
+                    {
+                        date: new Date('2012-01-01'),
+                        score: 3,
+                    },
+                    {
+                        date: new Date('2022-01-01'),
+                        score: 8,
+                    },
+                ],
+            },
+        ],
+        (error, result) => {
+            if (error) {
+                return console.log('Unable to insert restaurant');
+            }
+            console.log(result);
+        }
     );
 });
